138553dee80433e442cbec2c17fe69de4bcdecdd
urwah-fatima <92842201+urwah-fatima@users.noreply.github.com>
2024-11-04 18:05:13 +0500

    Update ContentBoxes.tsx

--- src/app/sections/featured-wroks/ContentBoxes.tsx
+++ src/app/sections/featured-wroks/ContentBoxes.tsx
@@ -1,3 +1,4 @@
+// Object containing the content for the Featured Works section
 const featuredWorksContent = {
   smHead1: "Featured Works",
   lgHead2: "Designing Dashboards",
@@ -8,23 +9,32 @@ const featuredWorksContent = {
     "officia consequat duis enim velit mollit. Exercitation veniam consequat " +
     "sunt nostrud amet.",
 };
+
+// Functional component for rendering Featured Works content
 const FeaturedWorksBoxes = () => {
   return (
     <>
-      <div className="mr-6">
+      {/* Main container for the featured work item */}
+      <div className="mr-6 ml-10">
+        {/* Title of the featured work */}
         <h1 className="text-black font-semibold font-heebo text-lg">
           {featuredWorksContent.lgHead2}
         </h1>
+        {/* Container for year and category */}
         <div className="flex justify-normal">
+          {/* Year display with custom styling */}
           <span className="text-white font-bold font-heebo bg-heroButtonPink rounded-xl px-4 py-0">
-            {featuredWorksContent.p1}
-          </span>
+            {featuredWorksContent.p1}</span>
+          
+          {/* Category display */}
           <h5 className="text-gray-700 ml-3">{featuredWorksContent.p2}</h5>
         </div>
+        {/* Description of the featured work */}
         <p className="text-gray-700 font-heebo">{featuredWorksContent.p3}</p>
       </div>
     </>
   );
 };
 
+// Export the component for use in other parts of the application
 export default FeaturedWorksBoxes;
